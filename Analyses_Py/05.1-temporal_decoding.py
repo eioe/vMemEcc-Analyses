9d5a4d7b49f34c287442b5dd00938472aace632e
felix.klotzsche <klotzsche@cbs.mpg.de>
2020-11-24 19:33:36 +0100

    extract decode method

--- Analyses_Py/05.1-temporal_decoding.py
+++ Analyses_Py/05.1-temporal_decoding.py
@@ -1,10 +1,8 @@
 
 
-#%% load libs:
+# %% load libs:
 from collections import defaultdict
 from os import path as op
-from mne.decoding.base import LinearModel
-from mne.epochs import concatenate_epochs
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -14,33 +12,35 @@ from sklearn.linear_model import LogisticRegression
 
 import mne
 from mne import EvokedArray
-from mne.decoding import (SlidingEstimator, GeneralizingEstimator,
+# from mne.epochs import concatenate_epochs
+from mne.decoding import (SlidingEstimator,  # GeneralizingEstimator,
                           cross_val_multiscore, LinearModel, get_coef)
 
 from library import config, helpers
 
 
-#%% Functions:
+# %% Functions:
 
-def get_epos(subID, epo_part, signaltype, cond, event_dict):
+def get_epos(subID, epo_part, signaltype, condition, event_dict):
     if signaltype == 'uncollapsed':
         fname = op.join(config.path_rejepo, subID + '-' + epo_part +
                         '-postica-rejepo' + '-epo.fif')
     elif signaltype in ['collapsed', 'difference']:
-        fname = op.join(config.path_epos_sorted, epo_part, signaltype, 
+        fname = op.join(config.path_epos_sorted, epo_part, signaltype,
                         subID + '-epo.fif')
     else:
         raise ValueError(f'Invalid value for "signaltype": {signaltype}')
     epos = mne.read_epochs(fname, verbose=False)
     epos = epos.pick_types(eeg=True)
-    uppers = [l.isupper() for l in cond]
-    if (np.sum(uppers) > 2): 
-        cond_1 = cond[:np.where(uppers)[0][2]]
-        cond_2 = cond[np.where(uppers)[0][2]:]
+    uppers = [letter.isupper() for letter in condition]
+    if (np.sum(uppers) > 2):
+        cond_1 = condition[:np.where(uppers)[0][2]]
+        cond_2 = condition[np.where(uppers)[0][2]:]
         selection = epos[event_dict[cond_1]][event_dict[cond_2]]
     else:
-        selection = epos[event_dict[cond]]
+        selection = epos[event_dict[condition]]
     return(selection)
+
 
 def avg_time(data, step=25, times=None):
     orig_shape = data.shape
@@ -70,21 +70,23 @@ def batch_trials(epos, batch_size):
     return(batches)
 
 
-def get_data(subID, conditions, batch_size=1, smooth_winsize=1):
+def get_data(subID, epo_part, signaltype, conditions, event_dict,
+             batch_size=1, smooth_winsize=1):
     epos_dict = defaultdict(dict)
     for cond in conditions:
         epos_dict[cond] = get_epos(subID,
-                                   'stimon',
-                                   cond,
-                                   event_dict) 
+                                   epo_part=epo_part,
+                                   signaltype=signaltype,
+                                   condition=cond,
+                                   event_dict=event_dict)
 
-    times = epos_dict[cond][0].copy().times
+    times = epos_dict[conditions[0]][0].copy().times
 
-    # Setup decoding pipeline:
+    # Setup data:
     if batch_size > 1:
         batches = defaultdict(list)
         for cond in conditions:
-            batches[cond] = batch_trials(epos_dict[cond], 15)
+            batches[cond] = batch_trials(epos_dict[cond], batch_size)
             batches[cond] = np.asarray([b.data for b in batches[cond]])
 
         X = np.concatenate([batches[cond].data for cond in conditions], axis=0)
@@ -96,18 +98,91 @@ def get_data(subID, conditions, batch_size=1, smooth_winsize=1):
         n_ = {cond: len(epos_dict[cond]) for cond in conditions}
 
     if smooth_winsize > 1:
-        X, times_n = avg_time(X, 25, times=times)
+        X, times_n = avg_time(X, smooth_winsize, times=times)
     else:
         times_n = times
 
     y = np.r_[np.zeros(n_[conditions[0]]),
-              np.concatenate([(np.ones(n_[conditions[i]]) * i) 
+              np.concatenate([(np.ones(n_[conditions[i]]) * i)
                               for i in np.arange(1, len(conditions))])]
 
     return X, y, times_n
 
 
-#%% setup params:
+def decode(sub_list_str, conditions, epo_part='stimon', signaltype='collapsed',
+           event_dict=config.event_dict, n_rep_sub=100,
+           batch_size=10, smooth_winsize=5, save_single_rep=False,
+           save_patterns=False):
+
+    contrast_str = '_vs_'.join(conditions)
+
+    clf = make_pipeline(StandardScaler(),
+                        LinearModel(LogisticRegression(solver='liblinear',
+                                                       penalty='l2',
+                                                       random_state=42)))
+
+    se = SlidingEstimator(clf,
+                          scoring='accuracy',
+                          n_jobs=-2,
+                          verbose='warning')
+
+    sub_results = list()
+    sub_coef = list()
+
+    for sub in sub_list_str:
+        count = sub_list_str.index(sub)
+        print(f'### RUNING SUBJECT {sub}')
+        all_scores = list()
+        all_coef = list()
+        for i in np.arange(n_rep_sub):
+            X, y, times_n = get_data(sub,
+                                     epo_part=epo_part,
+                                     signaltype=signaltype,
+                                     conditions=conditions,
+                                     event_dict=event_dict,
+                                     batch_size=batch_size,
+                                     smooth_winsize=smooth_winsize)
+            scores = cross_val_multiscore(se, X=X, y=y, cv=3, verbose='warning')
+            scores = np.mean(scores, axis=0)
+            all_scores.append(scores)
+            se.fit(X, y)
+            coef = get_coef(se, 'patterns_', inverse_transform=True)
+            all_coef.append(coef)
+
+        avg_scores = np.asarray(all_scores).mean(axis=0)
+        sub_results.append(avg_scores)
+        sub_coef.append(np.asarray(all_coef).mean(axis=0))
+
+        # save shizzle:
+        if save_single_rep:
+            if count == 0:
+                sub_scores_per_rep = np.asarray(all_scores)
+            else:
+                sub_scores_per_rep = np.concatenate([sub_scores_per_rep,
+                                                    np.asarray(all_scores)],
+                                                    axis=0)
+
+            fpath = op.join(config.path_decod_temp, contrast_str, 'single_rep_data')
+            helpers.chkmk_dir(fpath)
+            fname = op.join(fpath,
+                            f'reps{n_rep_sub}_' \
+                            f'swin{smooth_winsize}_batchs{batch_size}.npy')
+            np.save(fname, sub_scores_per_rep)
+            del(fpath, fname)
+
+        # save patterns:
+        if save_patterns:
+            sub_patterns = np.asarray(sub_coef)
+            fpath = op.join(config.path_decod_temp, contrast_str, 'patterns')
+            helpers.chkmk_dir(fpath)
+            fname = op.join(fpath, 'patterns_per_sub.npy')
+            np.save(fname, sub_patterns)
+            del(fpath, fname)
+
+    return sub_results, sub_coef
+
+
+# %% setup params:
 
 
 # plotting:
@@ -123,7 +198,7 @@ cond_dict = {'Load': ['LoadLow', 'LoadHigh'],
              'Ecc': ['EccS', 'EccM', 'EccL']}
 
 
-#%% load data:
+# %% hans
 
 
 sub_list = np.setdiff1d(np.arange(1, 28), config.ids_missing_subjects +
@@ -139,72 +214,6 @@ smooth_winsize = 5
 n_rep_sub = 1
 save_single_rep = False
 save_patterns = False
-
-
-
-
-clf = make_pipeline(StandardScaler(),
-                    LinearModel(LogisticRegression(solver='liblinear',
-                                                   penalty='l2', 
-                                                   random_state=42)))
-
-se = SlidingEstimator(clf,
-                      scoring='accuracy',
-                      n_jobs=-2,
-                      verbose='warning')
-
-sub_results = list()
-sub_coef = list()
-
-for sub in sub_list_str:
-    count = sub_list_str.index(sub)
-    print(f'### RUNING SUBJECT {sub}')
-    all_scores = list()
-    all_coef = list()
-    for i in np.arange(n_rep_sub):
-        X, y, times_n = get_data(sub,
-                                 conditions=conditions, #cond_dict['Load']
-                                 batch_size=batch_size,
-                                 smooth_winsize=smooth_winsize)
-        scores = cross_val_multiscore(se, X=X, y=y, cv=5, verbose='warning')
-        scores = np.mean(scores, axis=0)
-        all_scores.append(scores)
-        # get 
-        se.fit(X, y)
-        coef = get_coef(se, 'patterns_', inverse_transform=True)
-        all_coef.append(coef)
-
-    avg_scores = np.asarray(all_scores).mean(axis=0)
-    sub_results.append(avg_scores)
-    sub_coef.append(np.asarray(all_coef).mean(axis=0))
-
-    # save shizzle:
-    if save_single_rep:
-        if count == 0:
-            sub_scores_per_rep = np.asarray(all_scores)
-            #sub_scores_per_rep = np.expand_dims(sub_scores_per_rep, 0)
-        else:
-            sub_scores_per_rep = np.concatenate([sub_scores_per_rep, 
-                                                 np.asarray(all_scores)], 
-                                                axis=0)
-
-        fpath = op.join(config.path_decod_temp, contrast_str, 'single_rep_data')
-        helpers.chkmk_dir(fpath)
-        fname = op.join(fpath,
-                        f'reps{n_rep_sub}_' \
-                        f'swin{smooth_winsize}_batchs{batch_size}.npy')
-        np.save(fname, sub_scores_per_rep)
-        del(fpath, fname)
-
-    # save patterns:
-    if save_patterns:
-        sub_patterns = np.asarray(sub_coef)
-        fpath = op.join(config.path_decod_temp, contrast_str, 'patterns')
-        helpers.chkmk_dir(fpath)
-        fname = op.join(fpath, 'patterns_per_sub.npy')
-        np.save(fname, sub_patterns)
-        del(fpath, fname)
-    
 
 #%%
 
