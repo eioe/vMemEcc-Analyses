f67dce06ba5580f9224fe71dd8c8291dd8c3f990
felix.klotzsche <klotzsche@cbs.mpg.de>
2020-12-21 13:34:47 +0100

    clean local

--- Analyses_Py/_tmp.py
+++ Analyses_Py/_tmp.py
@@ -1,9 +1,10 @@
+import streamlit
+import numpy as np
+import matplotlib.pyplot as plt
 
 
-evo_s = list()
-evo_m = list()
-evo_l = list()
-for sub in sub_list_str: 
-    evo_s.append(get_epos(sub, 'stimon', 'LoadLow', event_dict).average())
-    evo_m.append(get_epos(sub, 'stimon', 'LoadHigh', event_dict).average())
-    evo_l.append(get_epos(sub, 'stimon', 'EccL', event_dict).average())
+x = np.arange(100)
+
+y = np.random.choice(100, 100)
+
+plt.plot(x, y)
