93ac732805e5c000a65eac87dd3097f256884627
felix.klotzsche <klotzsche@cbs.mpg.de>
2021-06-10 14:15:28 +0200

    fix ppid for S07; add filter for trials wit saccade

--- Analyses_R/Utils/load_data.R
+++ Analyses_R/Utils/load_data.R
@@ -6,16 +6,15 @@
 #TODO: make function and explicitely return specific df
 
 # excluded subjects:
-#QUESTION: exclude these before saving RDS?
 excl_subs <- c('VME_S11', 'VME_S14', 'VME_S19', # incomplete data
-               'VME_S22', 'VME_S7', 'VME_S12')  # bad EEG
+               'VME_S22', 'VME_S07', 'VME_S12')  # bad EEG
 
 fname <- file.path(path_r_data, 'fulldat_behav.rds')
 data_full <- readRDS(fname) %>% 
   filter(!ppid %in% excl_subs)
 rm(fname)
 
-#TODO: Implement filters: DroppedFrames!, BadEEGTrials?, IncorrectTrials?
+#TODO: Implement filters: DroppedFrames
 
 data_behav <- data_full %>% 
   filter(BlockStyle %in% c('perception', 'experiment')) %>% 
@@ -27,6 +26,33 @@ data_behav <- data_full %>%
          c_ResponseCorrect, 
          c_ResponseTime, 
          BlockStyle)
+
+# Filter rout trials in which a (large) saccade was detected:
+path_ET_rej_trials <- file.path(path_global, 
+                                'Data', 
+                                'DataMNE', 
+                                'EEG', 
+                                '05.1_rejepo', 
+                                'CSV_rejEpos_ET')
+files <- list.files(path_ET_rej_trials)
+
+rej_epos_per_sub <- list()
+for (file in files) {
+  sub_id <- str_split(file, '-')[[1]][1]
+  fpath <- file.path(path_ET_rej_trials, file)
+  rej_epos <- read_csv(fpath, col_names='trial_num')
+  rej_epos_per_sub[[sub_id]] <- rej_epos
+}
+
+rej_epos_df <- bind_rows(rej_epos_per_sub, .id='ppid') %>%  
+  mutate('ET_sacc_detected'=TRUE, BlockStyle = 'experiment')
+
+
+data_behav <- left_join(data_behav, rej_epos_df, by = c('ppid', 'trial_num')) %>% 
+  mutate(ET_sacc_detected = if_else(is.na(ET_sacc_detected), FALSE, TRUE)) %>% 
+  filter(!ET_sacc_detected) %>% 
+  select(!ET_sacc_detected)
+
 
 ##-----------------------------------------------------------------------
 # Read in CDA mean amplitudes:
