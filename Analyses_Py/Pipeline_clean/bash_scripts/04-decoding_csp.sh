2c9bbb76c4f469970fdf52d2a08d7849fc2797e4
Felix Klotzsche <klotzsche@cbs.mpg.de>
2022-04-30 16:00:22 +0200

    major clean up of pipeline; RENAMING many files;

--- Analyses_Py/Pipeline_clean/bash_scripts/04-decoding_csp.sh
+++ Analyses_Py/Pipeline_clean/bash_scripts/04-decoding_csp.sh
@@ -1,30 +1,30 @@
 #!/bin/bash -l 
 # +
 # specify the indexes of the job array elements
-#SBATCH --array=0-26 
+#SBATCH --array=0-21
 # Standard output and error: 
 #SBATCH -o ./job.out.%j        # Standard output, %A = job ID, %a = job array index 
 #SBATCH -e ./job.err.%j        # Standard error, %A = job ID, %a = job array index 
 # Initial working directory: 
 #SBATCH -D /ptmp/fklotzsche/Experiments/vMemEcc/
 # Job Name: 
-#SBATCH -J decoding_sensorspace
+#SBATCH -J decoding_csp
 # Queue (Partition): 
 #SBATCH --partition=general 
 # Number of nodes and MPI tasks per node: 
 #SBATCH --nodes=1 
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=50 
+#SBATCH --cpus-per-task=70
 #SBATCH --mem=20000
 
 #SBATCH --mail-type=all 
 #SBATCH --mail-user=klotzsche@cbs.mpg.de 
 
-#SBATCH --time=01:00:10
+#SBATCH --time=14:00:00
 # -
 
 module load anaconda/3/2021.05
 conda activate mne
 
 # Run the program: 
-srun python ./vme_analysis/Analyses_Py/--04.0-decoding_sensorspace.py $SLURM_ARRAY_TASK_ID            #02.1_preprocess_data_batch.py $SLURM_ARRAY_TASK_ID
+srun python ./vme_analysis/Analyses_Py/04.3-decoding_csp.py $SLURM_ARRAY_TASK_ID
