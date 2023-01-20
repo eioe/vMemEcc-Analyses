a346486ce141a7965a3bd60773bbf29880056ecd
felix.klotzsche <klotzsche@cbs.mpg.de>
2023-01-20 20:13:22 +0100

    clean up R folders

--- Analyses_R/Utils/load_packages.R
+++ Analyses_R/Utils/load_packages.R
@@ -8,6 +8,18 @@ library(lmerTest)
 library(rjson)
 library(corrplot)
 
+library(parallel)
+library(ggplot2)
+library(reshape)
+library(pracma)
+library(grid)
+library(ez)				# includes Greenhouse Geisser correction for ANOVA
+#library(BayesFactor)
+#library(viridis)
+library(lme4)
+library(nlme)
+#library(patchwork)
+
 
 source(here("Utils", "wrangle_data.R"))
 source(here("Utils", "plot_behavior.R"))
